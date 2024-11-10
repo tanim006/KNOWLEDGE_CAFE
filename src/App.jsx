b299aaf390f666a0b7861ad0893c094bcd46df3c
tanim006 <tanimahamed680@gmail.com>
2024-11-10 10:28:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
